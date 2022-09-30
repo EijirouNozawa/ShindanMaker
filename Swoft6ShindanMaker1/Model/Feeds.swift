b6613bc005984851a905a0086450766d4c090084
EijirouNozawa <sacjapan2009@gmail.com>
2022-09-30 16:06:06 +0900

    ShindanMaker

---
+++ Swoft6ShindanMaker1/Model/Feeds.swift
@@ -0,0 +1,18 @@
+//
+//  Feeds.swift
+//  Swoft6ShindanMaker1
+//
+//  Created by 野澤英二郎 on 2021/03/05.
+//
+
+import Foundation
+
+struct Feeds {
+    
+    let userName:String
+    let quote:String
+    let profileURL:String
+    
+    
+    
+}
